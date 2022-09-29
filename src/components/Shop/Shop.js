4e8237911bbc0c5c7be67bd5c1a40d7b805b6cc8
hridoyhalder1 <hridoyhalder91@gmail.com>
2022-09-29 16:31:34 +0600

    profile

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -26,7 +26,29 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <h1>Profile summary</h1>
+                <h1>Hriday Haldar</h1>
+                <small>Dhaka, Bangladesh</small>
+                <div className='detail-info'>
+                    <h3>75kg Weight</h3>
+                    <h3>6.5 Height</h3>
+                    <h3>25 year</h3>
+                </div>
+                <h2>Add a Break</h2>
+                <div className='break'>
+                    <h5>10s</h5>
+                    <h5>20s</h5>
+                    <h5>30s</h5>
+                    <h5>40s</h5>
+                </div>
+                <h2>Exercise Details</h2>
+                <div className='exercise-detail'>
+                    <input type="text" name="" id="" placeholder='Exercise time 200 seconds' />
+                    <h3>Break time:</h3>
+                    <input type="text" name="" id="" placeholder='Break time 15 seconds' />
+                </div>
+                <button className='activity-btn'>
+                    <p>Activity Completed</p>
+                </button>
             </div>
         </div>
     );
