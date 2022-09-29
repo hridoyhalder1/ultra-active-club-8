20896420515a967d679252c12b18e7e6ad557037
hridoyhalder1 <hridoyhalder91@gmail.com>
2022-09-29 15:33:10 +0600

    card added

---
+++ src/components/Product/Product.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Product.css';
+
+const Product = (props) => {
+    // console.log(props);
+    const {picture, name, blog, time, age} = props.product
+    return (
+        <div className='product'>
+            <img src={picture} alt=''></img>
+            <div className='product-info'>
+                <h3 className='product-name'>{name}</h3>
+                <p className='blog'>{blog}</p>
+                <p>For Age: {age}</p>
+                <p>Time required: {time}</p>
+            </div>
+            <button className="btn-cart">
+                <p>Add To List</p>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from '../../Images/logo.png';
+import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    },[])
     return (
         <div className='shop-container'>
+            <div className='logo'>
+                <img src={logo} alt=''></img>
+                <h2>Neuro Exercise</h2>
+            </div>
+            <h3>Select Today's Exercise:{products.length}</h3>
             <div className="products-container">
-                <div className='logo'>
-                    <img src={logo} alt=''></img>
-                    <h2>Neuro Exercise</h2>
-                </div>
-                <h3>Select Today's Exercise</h3>
+                {
+                    products.map(product => <Product 
+                        key={product.id}
+                        product={product}
+                        ></Product>)
+                }
             </div>
             <div className="cart-container">
                 <h1>Profile summary</h1>
